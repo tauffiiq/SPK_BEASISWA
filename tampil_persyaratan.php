4cf13dd4c819f3eca5fd4c26a370d6d22ff15517
LAPTOP-58QPTVQ6\Lenovo <tauffiiiq@gmail.com>
2021-12-14 22:29:53 +0700

    Revert "sistem pengambil keputusan beasiswa"
    This reverts commit 7f67713fb1935d2574fcdf183665743414b31092.

--- tampil_persyaratan.php
+++
@@ -1,3 +0,0 @@
-<h2 align="center">PERSYARATAN BEASISWA</h2>
-<a class="btn btn-primary" href="?page=pendaftaran" style="margin-bottom: 30px">Back</a>
-
--- welcome.php
+++
@@ -1,4 +0,0 @@
-<h1 align="center">SELAMAT DATANG</h1>
-
-<h3 align="center">SISTEM PEMBUAT KEPUTUSAN BEASISWA</h3>
-
